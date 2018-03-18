7ef0a933af0aabebf6973698ddd51ec176a6ea91
Albert Co <trblae@gmail.com>
2018-03-18 14:20:58 -0400

    Increase # of LEDs to 6

--- keyboards/single_switch/config.h
+++ keyboards/single_switch/config.h
@@ -58,7 +58,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLIGHT_TIMER
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 4    // Number of LEDs
+#define RGBLED_NUM 6    // Number of LEDs
 #define ws2812_PORTREG  PORTD
 #define ws2812_DDRREG   DDRD
 
